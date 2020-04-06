40724142dc048da2e864a1323b77b015d05c28d9
Bart Le <le.bartlomiej@gmail.com>
2020-04-06 05:07:32 +0200

    Log user and set the actual username

--- src/Scraper.js
+++ src/Scraper.js
@@ -18,6 +18,24 @@ class Scraper {
 		await this.goToPage("https://www.filmweb.pl/login");
 	};
 
+	async login(usernameOrEmail, password) {
+		await this.page.waitForSelector(".rodoBoard", { visible: true });
+		await this.page.click(".fwBtn--gold");
+		await this.page.click(".authbutton--filmweb");
+
+		await this.page.type("[name='j_username']", usernameOrEmail);
+		await this.page.type("[name='j_password']", password);
+		await this.page.click(".authButton--submit");
+		await this.setUsername();
+	}
+
+	async setUsername() {
+		await this.page.waitForSelector("#userAvatar > a", { visible: true });
+
+		this.username = await this.page.$$eval("#userAvatar > a", a =>
+			a.map(href => href.getAttribute("href")));
+	}
+
 	async goToPage(url) {
 		await this.page.goto(url, { waitUntil: "networkidle2" });
 	}
@@ -28,6 +46,7 @@ class Scraper {
 
 	async init() {
 		await this.setBrowser();
+		await this.login(this.username, this.password);
 		await this.closeBrowser();
 	}
 }

6f02331a6a834daae5ab6c65237ed7a9f9e719ca
Bart Le <le.bartlomiej@gmail.com>
2020-04-06 04:49:15 +0200

    Log messages if wrong arguments

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 const params = process.argv.slice(2);
 const [usernameOrEmail, password] = params;
 
-console.log(usernameOrEmail, password);
+(async () => {
+	if (params.length === 2) {
+		try {
+			console.log(usernameOrEmail, password);
+		} catch (e) {
+			console.log("Invalid credentials.");
+		}
+	} else {
+		console.log("Try again using two parameters. First as login and second as your password.")
+	}
+})();
